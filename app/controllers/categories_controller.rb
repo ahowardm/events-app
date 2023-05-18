51151aaa7d79020ab02b7c35d4c4b532442cae5c
Andrés Howard <ahowardm@gmail.com>
2023-05-18 08:27:08 -0400

    feat: clase pasada controladores y vistas

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -4,6 +4,19 @@ class CategoriesController < ApplicationController
     end
 
     def show
-        
+        @category = Category.find params[:id]
+    end
+
+    def new
+        @category = Category.new
+    end
+
+    def create
+        cat = Category.new( name: params[:category][:name])
+        if cat.save
+            redirect_to cat, notice: 'Categoría creada con éxito'
+        else
+            redirect_to new_category_path, alert: 'No se pudo crear la categoría'
+        end
     end
 end

59a76b3bfc9d6fe01de84d7e437fd3324e6513a2
Andrés Howard <ahowardm@gmail.com>
2023-05-24 21:48:38 -0400

    feat: agregar dashboard con ajax

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -3,6 +3,10 @@ class CategoriesController < ApplicationController
 
     def index
         @categories = Category.all
+        respond_to do |format|
+            format.html {render :index, layout: !request.xhr? }
+            format.json { render json: @categories}
+        end
     end
 
     def show
